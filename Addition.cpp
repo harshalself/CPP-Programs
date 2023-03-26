c3a1e37a6961fb945d9afaf456e372b97fbaf9ec
Harshal <harshalpatilself@gmail.com>
2023-03-26 12:29:15 +0530

    First Commit

---
+++ Addition.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+
+    cout<<"\n Enter First No : ";
+    cin>>a;
+
+    cout<<"\n Enter Second No : ";
+    cin>>b;
+
+    c=a+b;
+
+    cout<<"\n Sum is "<<c;   
+    
+    return 0;
+}

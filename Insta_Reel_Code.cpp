87086e0ff64ff7d10cfed2bb53257f3c4f42dc45
Harshal <harshalpatilself@gmail.com>
2023-03-28 21:14:29 +0530

    nf: Insta_Reel_Code.cpp

---
+++ Insta_Reel_Code.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string.h>
+#include<conio.h>
+#include<windows.h>
+
+using namespace std;
+
+int main()
+{
+    char a[]=" HELLO MY NAME IS HARSHAL \n This is My Official Account \n Here I will be Posting Content Related to \n Coding, Photography and More such Things \n Do Follow and \n Don't forget to Like \n THANK YOU ";
+    int i,n;
+
+    n=strlen(a);
+    
+    for(i=0;i<n;i++)
+    {
+        Sleep(100);
+        cout<<""<<a[i];
+    }
+
+    return 0;
+}
